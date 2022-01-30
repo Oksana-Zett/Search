de12f923f317742be760b01f3ebb22e9907a829c
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2022-01-30 17:07:43 +0300

    Update TicketManagerTest

--- src/test/java/manager/TicketManagerTest.java
+++ src/test/java/manager/TicketManagerTest.java
@@ -18,10 +18,34 @@ class TicketManagerTest {
     Ticket ticket6 = new Ticket(6, 9999, "LGW", "CDG", 65);
     Ticket ticket7 = new Ticket(7, 14896, "VKO", "CDG", 300);
     Ticket ticket8 = new Ticket(8, 7540, "CDG", "VKO", 75);
+    Ticket ticket9 = new Ticket(9, 17201, "VKO", "DXB", 325);
+    Ticket ticket10 = new Ticket(10, 10717, "LGW", "CDG", 80);
+    Ticket ticket11 = new Ticket(11, 17202, "VKO", "DXB", 325);
+    Ticket ticket12 = new Ticket(12, 10718, "LGW", "CDG", 80);
+    Ticket ticket13 = new Ticket(13, 16999, "VKO", "DXB", 300);
+
+    @Test
+    void shouldSearchByEmpty() {
+
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.searchByAirport("VKO", "DXB");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSearchByOneTicket() {
+        manager.add(ticket1);
+
+        Ticket[] expected = new Ticket[]{ticket1};
+        Ticket[] actual = manager.searchByAirport("VKO", "DXB");
+
+        assertArrayEquals(expected, actual);
+    }
 
 
     @Test
-    void shouldSearchByAirport() {
+    void shouldSearchBySomeTickets() {
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
@@ -30,8 +54,13 @@ class TicketManagerTest {
         manager.add(ticket6);
         manager.add(ticket7);
         manager.add(ticket8);
+        manager.add(ticket9);
+        manager.add(ticket10);
+        manager.add(ticket11);
+        manager.add(ticket12);
+        manager.add(ticket13);
 
-        Ticket[] expected = new Ticket[]{ticket1, ticket5, ticket3};
+        Ticket[] expected = new Ticket[]{ticket13, ticket1, ticket9, ticket11, ticket5, ticket3};
         Ticket[] actual = manager.searchByAirport("VKO", "DXB");
 
         assertArrayEquals(expected, actual);
